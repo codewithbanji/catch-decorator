f073058b80989585644854b14abe969a1214845c
codewithbanji <63445861+codewithbanji@users.noreply.github.com>
2020-07-12 10:30:54 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-type HandlerFunction = (error: any, ctx: any) => void;
+
+// originaly by https://github.com/enkot/catch-decorator/blob/master/src/index.ts
+
+type HandlerFunction = (error: any, ctx: any, args?: any[]) => void;
+type Finalizer = (ctx: any, args?: any[]) => void;
 
 function handleError(
-    ctx: any, 
-    errorClass: any, 
-    handler: HandlerFunction, 
-    error: any
+    ctx: any,
+    errorClass: any,
+    handler: HandlerFunction,
+    error: any,
+    args: any[]
 ) {
+
+
     // check if error is instance of passed error class
     if (typeof handler === 'function' && error instanceof errorClass) {
-        // run handler with error object 
+        // run handler with error object
         // and class context as second argument
-        handler.call(null, error, ctx)
+        handler.call(null, error, ctx, args)
     } else {
         // throw error further,
         // next decorator in chain can catch it
@@ -19,7 +26,7 @@ function handleError(
 }
 
 // decorator factory function
-export default (errorClass: any, handler: HandlerFunction): any => {
+export default (errorClass: any, handler: HandlerFunction, finalizer?: Finalizer): any => {
     return (
         target: any,
         propertyKey: string,
@@ -30,21 +37,28 @@ export default (errorClass: any, handler: HandlerFunction): any => {
 
         // rewrite original method with custom wrapper
         descriptor.value = function(...args: any[]) {
+            let isPromise = false;
             try {
                 const result = originalMethod.apply(this, args)
-                
+
                 // check if method is asynchronous
                 if (result && typeof result.then === 'function' && typeof result.catch === 'function') {
+                    isPromise = true;
                     // return promise
                     return result.catch((error: any) => {
-                        handleError(this, errorClass, handler, error)
-                    })
+                        handleError(this, errorClass, handler, error,args )
+                    // and the finally block here:
+                    }).finally(() => {if(finalizer) finalizer.call(null,this,args)});
                 }
 
                 // return actual result
                 return result
             } catch (error) {
-               handleError(this, errorClass, handler, error)
+               handleError(this, errorClass, handler, error, args)
+            } finally{
+              if(!isPromise){
+                if(finalizer) finalizer.call(null,args)
+              }
             }
         }
 
